70f030e084c1c63bc53b361e496eb04ace762af2
akx-repo <akx.kodi@bol.com.br>
2015-11-16 15:03:55 -0200

    filmesonlines series
    series corrigidas

--- plugin.video.filmesonlinegratis2/addon.py
+++ plugin.video.filmesonlinegratis2/addon.py
@@ -34,7 +34,7 @@ except:
 
 h = HTMLParser.HTMLParser()
 
-versao = '0.1.3'
+versao = '0.1.4'
 addon_id = 'plugin.video.filmesonlinegratis2'
 selfAddon = xbmcaddon.Addon(id=addon_id)
 addonfolder = selfAddon.getAddonInfo('path')
